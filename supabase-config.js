cfb13bbaddef1f490b50365ea5c308be0e1cdea1
theo117 <tednel3@gmail.com>
2026-06-05 12:28:01 +0200

    Update Supabase configuration with URL and anonKey

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.TRADEFLOW_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://rsvvvbhuvrzkdpgkbqas.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzdnZ2Ymh1dnJ6a2RwZ2ticWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NDYwMTcsImV4cCI6MjA5NjIyMjAxN30.AoOLAyorlnv9ZeBqY7J0dLBL6fnoEIXzd6X7kh_MZPw",
 };
